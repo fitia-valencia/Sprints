d0ea1179747e2f1cd648968710a14d154f24497e
fitia-valencia <fitivalenci@gmail.com>
2025-11-12 21:43:23 +0300

    sprint4

--- test-project/src/main/java/com/testapp/TestController2.java
+++ test-project/src/main/java/com/testapp/TestController2.java
@@ -7,7 +7,20 @@ import com.monframework.annotation.Route;
 public class TestController2 {
     
     @Route(value = "/dashboard")
-    public void showDashboard() {
+    public String showDashboard() {
         System.out.println("Tableau de bord admin - Controller2");
+        return "Bienvenue sur le tableau de bord administrateur";
+    }
+    
+    @Route(value = "/stats")
+    public Object getStats() {
+        System.out.println("Récupération des statistiques");
+        return "Statistiques: 1500 utilisateurs, 300 produits";
+    }
+    
+    @Route(value = "/null-method")
+    public String nullMethod() {
+        System.out.println("Méthode retournant null");
+        return null;
     }
 }
